2d48ff8c60921de320dddc125f5494560029e5dd
Amber <ambervyx@gmail.com>
2016-10-27 18:59:37 +0800

    Add files via upload

---
+++ Practice/challenge1.1.py
@@ -0,0 +1,16 @@
+#amp the difficulty--use a function
+from turtle import*
+
+def go():
+    fd(x)
+    left(120)
+    fd(x+10)
+    left(120)
+    
+x=10
+y=20
+
+while x<2000 and y<2000:
+    go()
+    x=x+20
+    y=x+40
---
+++ Practice/challenge1.2.py
@@ -0,0 +1,18 @@
+from turtle import*
+import math
+
+fd(300)
+left(90)
+fd(200)
+left(180-56.3)
+fd(math.sqrt(130000))
+right(180-56.3)
+fd(200)
+right(90)
+fd(300)
+left(135)
+fd(212.132)
+left(90)
+fd(212.132)
+left(45+56.3)
+fd(math.sqrt(130000))
